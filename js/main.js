5ff4e3ec72dfb6b7131f3d585ff4d4f287f0bb04
fernandojoselopes <joseferandolopes.2019.2@gmail.com>
2020-10-20 23:15:25 -0300

    incuido index.html, js/ e main.js

---
+++ js/main.js
@@ -0,0 +1,86 @@
+
+
+var lista = ["maçã", "pera", "laranja"];
+lista.push("uva"); //inclui mais intens
+lista.pop("pera");//remove o ultimo elemento da lista*/
+console.log(lista);
+
+
+
+//var d = new Date();
+//console.log("Hoje é " +d.getDate()+"/"+(d.getMonth()+1)); //data
+//alert(d.getHours()); //horas
+//alert(d.getMonth()+1); //mostra o mês
+//alert(d.getDate());//mostra o dia numergeico natual
+
+/*
+var contar;
+for (contar = 0; contar <=5; contar++) {
+    console.log(contar);
+    alert(contar);
+};
+*/
+
+
+/*
+var contar = 0;
+while (contar <= 5){
+    console.log(contar);
+    alert(contar);
+    //contar = contar + 1;
+    contar++;
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade")
+if (idade >=18){
+    alert("Maior de idade");
+}else {
+    alert("Menor de idade");
+}
+*/
+
+/*
+var frutas = [{nome:"maçã" , cor:"vermelha"}, {nome:"uva" , cor:"roxa"}] 
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*
+var fruta = {nome:"maçã" , cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+/*var lista = ["maçã", "pera", "laranja"];
+lista.push("uva","manga"); //inclui mais intens
+lista.pop()//remove o ultimo elemento da lista*/
+//console.log(lista);
+//console.log(lista.length);//ver aquantidades de intens da lista
+//console.log(lista.reverse()); //inprime a quantidade e os intens
+//console.log(lista.toString());//torna os elemento do arry em string
+//console.log(lista.join(" / "));//troca a virguls por outro caracter como separador
+//console.log(lista[0]);//imprime só maçã
+//alert(lista[0]);//imprime só maçã
+//console.log(lista[1]);//imprime só pera
+//alert(lista[1]);//imprime só pera
+//console.log(lista[2]);//imprime só laranja
+//alert(lista[2]);//imprime só laranja
+
+
+/*var nome = "josé Fernando Lopes";
+var idade = 48;
+var idade2 = 10;
+var frase ="Japão é o melhor time do mundo!"
+//alert(nome + " tem " + idade + " anos ");
+//duas barras comentario uma lina
+/* barra asterisco 
+esterisco barra comenta mais de uma linha*/ 
+//alert(idade + idade2);
+/*console.log(nome);
+console.log(idade + idade2);*/
+//console.log(frase);
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão" , "Brasil"));
+/*console.log(frase.toLocaleUpperCase());//todas as letras MAIUSCULAS
+console.log(frase.toLocaleLowerCase());//todas as letras menusculas*/
